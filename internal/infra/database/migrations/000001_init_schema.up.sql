1ef7c3f12c7ed5867f709d12e56468c5cfd71c21
guilherme-moura95 <junior.moura19@hotmail.com>
2025-07-12 01:12:07 -0300

    - Create TAble for emails

--- internal/infra/database/migrations/000001_init_schema.up.sql
+++ internal/infra/database/migrations/000001_init_schema.up.sql
@@ -22,8 +22,30 @@ CREATE TABLE IF NOT EXISTS user_sessions (
                                              FOREIGN KEY (user_uuid) REFERENCES users(uuid) ON DELETE CASCADE
 );
 
+CREATE TABLE IF NOT EXISTS emails (
+                                      uuid         UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+                                      to_email     VARCHAR(255) NOT NULL,
+                                      subject      VARCHAR(255) NOT NULL,
+                                      body         TEXT NOT NULL,
+                                      type         VARCHAR(50) NOT NULL,
+                                      status       VARCHAR(50) NOT NULL DEFAULT 'pending',
+                                      attempts     INTEGER NOT NULL DEFAULT 0,
+                                      max_attempts INTEGER NOT NULL DEFAULT 3,
+                                      error_msg    TEXT,
+                                      sent_at      TIMESTAMPTZ,
+                                      created_at   TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+                                      updated_at   TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+
+
+
 -- Índices essenciais
 CREATE INDEX idx_users_email ON users(email);
 CREATE INDEX idx_users_email_password ON users(email, password);
 CREATE INDEX idx_user_sessions_user_uuid ON user_sessions(user_uuid);
 CREATE INDEX idx_user_sessions_refresh_token ON user_sessions(refresh_token);
+-- Índices para performance
+CREATE INDEX idx_emails_status ON emails(status);
+CREATE INDEX idx_emails_type ON emails(type);
+CREATE INDEX idx_emails_to_email ON emails(to_email);
+CREATE INDEX idx_emails_created_at ON emails(created_at);
